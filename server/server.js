34e8491e5eaa011ad67ed5d5fd535705757464ae
parthyeram <parthyeram18@gmail.com>
2026-06-15 01:45:05 +0530

    Fix Express 5 catch-all route

--- server/server.js
+++ server/server.js
@@ -177,7 +177,7 @@ app.get('/', (req, res) => {
 });
 
 // Catch-all route
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
